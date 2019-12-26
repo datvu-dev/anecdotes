0a261bb2cd38166517550ef115e6eb1c68d70aad
Dat Vu <dothanhdat.vu@gmail.com>
2019-12-27 10:39:44 +1100

    Add a reducer for notification

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+const initialState = [
+    'This is a notification'
+]
+  
+  
+const notificationReducer = (state = initialState, action) => {
+    return state
+}
+  
+export default notificationReducer
