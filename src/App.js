37f7695ba57f1b0d90a70079e998d57d5379f6c0
Dat Vu <dothanhdat.vu@gmail.com>
2019-12-29 11:47:36 +1100

    Remove anecdote service from App component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
-import anecdoteService from './services/anecdotes'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = (props) => {
